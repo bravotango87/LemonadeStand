0fd403bf6d354086759338afcf17635ce151235d
Brad Toney <bravotango87@gmail.com>
2020-04-17 15:06:44 -0400

    added a list of the days to the weather class. want it to generate a random day for the customer. now working on weather conditions and temperature

--- Weather.cs
+++ Weather.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace LemonadeStand_3DayStarter
 {
-    class Weather
+   
+    public class Weather
     {
+        // member variables (Has)
+
+        List<string> day;
+        string condition;
+        double temparature;
+
+        // constructor (Spawner)
+
+        public Weather()
+        {
+            day = new List<string>();
+
+            day.Add("Monday");
+            day.Add("Tuesday");
+            day.Add("Wednesday");
+            day.Add("Thursday");
+            day.Add("Friday");
+        }
+
+        // member methods (What it does)
+
+
+
+
     }
+
+
+
+
 }
