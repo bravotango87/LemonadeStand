fcdb0d4ed4a1468771d75c022b8f185e0ae84dc9
Brad Toney <bravotango87@gmail.com>
2020-04-18 22:43:05 -0400

    decided to do a switch case instead of a list for the weather. my next goal is to add random weather to the days of the week to try to make it a little realistic. wishbme luck

--- Weather.cs
+++ Weather.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LemonadeStand_3DayStarter
 {
-   
+
     public class Weather
     {
         // member variables (Has)
@@ -18,18 +18,60 @@ namespace LemonadeStand_3DayStarter
         // constructor (Spawner)
 
         public Weather()
+        // per day of the week
         {
-            day = new List<string>();
+            int day = 0;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Today is Monday!");
+                    break;
+                case 2:
+                    Console.WriteLine("Today is Tuesday!");
+                    break;
+                case 3:
+                    Console.WriteLine("Today is Wednesday!");
+                    break;
+                case 4:
+                    Console.WriteLine("Today is Thursday!");
+                    break;
+                case 5:
+                    Console.WriteLine("Today is Friday!");
+                    break;
+                case 6:
+                    Console.WriteLine("Today is Saturday!");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday!");
+                    break;
 
-            day.Add("Monday");
-            day.Add("Tuesday");
-            day.Add("Wednesday");
-            day.Add("Thursday");
-            day.Add("Friday");
+
+            }
+                
+
+
+
+
         }
+
+
+        public Weather(string condition, double temperature)
+        {
+            this.condition = condition;
+            this.temparature = temperature;
+
+
+        }
+
+
 
         // member methods (What it does)
 
+        // Display Weather
+        
+
+        
+        
 
 
 
