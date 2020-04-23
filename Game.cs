c1454c70f0334517cf98358d8d26377100d34d8e
Brad Toney <bravotango87@gmail.com>
2020-04-22 21:59:03 -0400

    updated RunGame in the Game class so the program will run (hopefully).

--- Game.cs
+++ Game.cs
@@ -42,8 +42,12 @@ namespace LemonadeStandGame
 
         public void RunGame()
         {
-            MainMenu();
+            Console.WriteLine("This game is for 7 days, so plan accordingly !");
+            Console.Clear();
+            Console.WriteLine("Good Morning !");
             DisplayForecast();
+            MainMenu();
+            
 
         }
 
@@ -90,39 +94,36 @@ namespace LemonadeStandGame
     {
         Console.WriteLine("Welcome to the Lemonade Stand");
         Console.WriteLine("Please choose an option from the menu");
-        Console.WriteLine("Enter 1  to check weather");
-        Console.WriteLine("Enter 2 to check inventory");
-        Console.WriteLine("Enter 3 to check wallet");
-        Console.WriteLine("Enter 4 to make recipe");
-        Console.WriteLine("Enter 5 to check recipe");
-        Console.WriteLine("Enter 6 to go to stand");
+        
+        Console.WriteLine("Enter 1 to check inventory");
+        Console.WriteLine("Enter 2 to check wallet");
+        Console.WriteLine("Enter 3 to make recipe");
+        Console.WriteLine("Enter 4 to check recipe");
+        Console.WriteLine("Enter 5 to go to stand");
 
 
         string userInput = Console.ReadLine();
 
         switch (userInput)
         {
+            
             case "1":
-                Console.Clear();
-                Console.WriteLine("Todays weather is "); // current day's weather (random number)
-                break;
-            case "2":
                 Console.Clear();
                 Console.WriteLine("You currently have "); // amount of lemons,sugar,ice cubes,cups
                 break;
-            case "3":
+            case "2":
                 Console.Clear();
                 Console.WriteLine("You have "); // amount of money in wallet
                 break;
-            case "4":
+            case "3":
                 Console.Clear();
                 Console.WriteLine("Enter amount of lemons, sugar, icecubes and cuops you would like to use"); // enter item amount
                 break;
-            case "5":
+            case "4":
                 Console.Clear();
                 Console.WriteLine("Your lemonade contains "); // receipe 
                 break;
-            case "6":
+            case "5":
                 Console.WriteLine("Time to sell some lemonade !");
                 break;
 
